238db0dc1bd62b6ae62b14a5ea803b8295b8e99a
Matheus <matheuslucena@gmail.com>
2017-02-18 12:42:07 -0200

    added routes to follo and unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users , :controllers => { registrations: 'users/registrations' }
 
   shallow do
-    resources :users do
+    resources :users, only: [:index, :show] do
+      get 'follow'
+      get 'unfollow'
       resources :posts do
         resources :comments
       end
