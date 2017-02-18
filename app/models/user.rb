edb16a310a361f879da835e2eec11fbfacb44168
Matheus <matheuslucena@gmail.com>
2017-02-18 12:35:49 -0200

    added method to follow and unfollow users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+
+  def follow user_id
+    self.following = { 'users': [] } unless self.following
+    self.following['users'] << user_id
+    self.save
+  end
+
+  def unfollow user_id
+    self.following['users'].delete(user_id)
+    self.save
+  end
+
+  def get_following_posts
+    self.following['users'].each do |user_id|
+      following_posts = Post.where(user_id: user_id)
+    end
+  end
 end
