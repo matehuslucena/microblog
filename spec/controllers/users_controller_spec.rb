71d11112322455451d8eb6b12ea321a6410432d8
Matheus <matheuslucena@gmail.com>
2017-02-19 16:07:41 -0300

    added unit tests for user controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+  shared_examples 'must redirected to signin' do
+    it 'be redirected to signin' do
+      route
+      
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
+  describe 'GET #index' do
+    it 'must render the index view' do
+      get :index
+
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #follow' do
+    let(:user) { create :another_user }
+    let(:route) { get :follow, params:{user_id: user} }
+
+    context 'when user is not logged in' do
+      include_examples 'must redirected to signin'
+    end
+
+    context 'when user is logged in' do
+      sign_in
+
+      it 'let the user follow another user' do
+        route
+
+        expect(response).to redirect_to(users_path)
+      end
+    end
+  end
+
+  describe 'GET #unfollow' do
+    let(:user) { create :another_user }
+    let(:route) { get :unfollow, params:{user_id: user} }
+
+    context 'when user is not logged in' do
+      include_examples 'must redirected to signin'
+    end
+
+    context 'when user is logged in' do
+      sign_in
+
+      it 'let the user unfollow another user' do
+        route
+
+        expect(response).to redirect_to(users_path)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    let(:user) { create :another_user }
+
+    it 'must render show view' do
+      get :show, params:{ id: user }
+
+      expect(response).to render_template(:show)
+    end
+  end
+end
