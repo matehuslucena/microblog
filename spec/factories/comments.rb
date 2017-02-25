c0d37f9659eb97d3509c32295ad3fa9451e6bb16
Matheus <matheuslucena@gmail.com>
2017-02-25 15:19:01 -0300

    changed factory to include user

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :comment do
     name Faker::Name.name
     body Faker::Lorem.sentence(3)
+    association :user, factory: :user
     association :post, factory: :post
   end
 
