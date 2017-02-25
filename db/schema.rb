3aedb9608ee91eef1eef832ad27221b5d340f27e
Matheus <matheuslucena@gmail.com>
2017-02-25 13:52:07 -0300

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218123021) do
+ActiveRecord::Schema.define(version: 20170225152909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20170218123021) do
     t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
   end
 
