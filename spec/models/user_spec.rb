a315fad638d004b0d1aba4cf0b2813ff11eea5f3
Matheus <matheuslucena@gmail.com>
2017-02-25 21:32:03 -0300

    added test for the new method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,25 +5,41 @@ describe User, type: :model do
   let(:user_to_follow) { create :another_user, email: 't@t.com' }
   let!(:post) { create :post, user: user_to_follow }
 
-  it 'must follow user' do
-    user.follow(user_to_follow.id)
+  describe '.follow' do
+    it 'must follow user' do
+      user.follow(user_to_follow.id)
 
-    expect(user.following['users'].include?(user_to_follow.id)).to eq true
+      expect(user.following['users'].include?(user_to_follow.id)).to eq true
+    end
   end
 
-  it 'must unfollow user' do
-    user.following['users'] << user_to_follow.id
+  describe '.unfollow' do
+    it 'must unfollow user' do
+      user.following['users'] << user_to_follow.id
 
-    user.unfollow(user_to_follow.id)
+      user.unfollow(user_to_follow.id)
 
-    expect(user.following['users'].include?(user_to_follow.id)).to eq false
+      expect(user.following['users'].include?(user_to_follow.id)).to eq false
+    end
   end
 
-  it 'must get all posts of followings users' do
-    user.follow(user_to_follow.id)
+  describe '.following_users_posts' do
+    it 'must get all posts of followings users' do
+      user.follow(user_to_follow.id)
 
-    posts = user.following_users_posts
+      posts = user.following_users_posts
 
-    expect(posts[0].first.id).to eq user_to_follow.posts[0].id
+      expect(posts[0].first.id).to eq user_to_follow.posts[0].id
+    end
+  end
+
+  describe '.search' do
+    it 'must search a user by name' do
+      user_name = user.name
+      
+      user_finded = User.search(user_name)
+
+      expect(user_finded.first.name).to eq user_name
+    end
   end
 end
